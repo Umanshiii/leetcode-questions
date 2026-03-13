9aea7a51f8bd7ac03f9c9a3e9f4038c1641489d8
Umanshiii <umanshi121@gmail.com>
2026-03-13 23:19:58 +0530

    Update largest rectangle.py

--- DP/largest rectangle.py
+++ DP/largest rectangle.py
@@ -1,14 +1,14 @@
-h=[3,2,3]
-stack=[]
-max_area=0
-for i in range(len(h)):
-    while stack and h[stack[-1]]>h[i]:
-        height=h[stack.pop()]
-        width=i if not stack else i-stack[-1]-1
-        max_area=max(max_area,height*width)
-    stack.append(i)
-while stack:
-    height=h[stack.pop()]
-    width=len(h) if not stack else len(h)-stack[-1]-1
-    max_area=max(max_area,height*width)
-print(max_area)
+def largestRectangle(h):
+    n = len(h)
+    ans = 0
+
+    for i in range(n):
+        height = h[i]
+        width = 0
+
+        for j in range(i, n):
+            height = min(height, h[j])
+            width += 1
+            ans = max(ans, height * width)
+
+    return ans
